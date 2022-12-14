eec38021301ad2faf512f8bcefe4251819f7781a
abrar_ol <abrar_ol@hotmail.com>
2022-12-14 10:20:05 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useRef } from "react";
+import useVideoPlayer from "./hooks/useVideoPlayer";
+import video from "./assets/video.mp4";
 
 function App() {
+  const videoElement = useRef(null);
+  const {
+    playerState,
+    togglePlay,
+    handleOnTimeUpdate,
+    handleVideoProgress,
+    handleVideoSpeed,
+    toggleMute,
+  } = useVideoPlayer(videoElement);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="video-wrapper">
+        <video
+          src={video}
+          ref={videoElement}
+          onTimeUpdate={handleOnTimeUpdate}
+        />
+
+        <div className="controls">
+          <div className="actions">
+            <button onClick={togglePlay}>
+              {!playerState.isPlaying ? (
+                <i className="bx bx-play"></i>
+              ) : (
+                <i className="bx bx-pause"></i>
+              )}
+            </button>
+          </div>
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value={playerState.progress}
+            onChange={(e) => handleVideoProgress(e)}
+          />
+         <select
+            className="velocity"
+            value={playerState.speed}
+            onChange={(e) => handleVideoSpeed(e)}
+          >
+            <option value="0.50">&nbsp;&nbsp;0.50x&nbsp;&nbsp;</option>
+            <option value="1">&nbsp;&nbsp;1x&nbsp;&nbsp;</option>
+            <option value="1.25">&nbsp;&nbsp;1.25x&nbsp;&nbsp;</option>
+            <option value="2">&nbsp;&nbsp;2x&nbsp;&nbsp;</option>
+          </select>
+
+          <button className="mute-btn" onClick={toggleMute}>
+            {!playerState.isMuted ? (
+              <i className="bx bxs-volume-full"></i>
+            ) : (
+              <i className="bx bxs-volume-mute"></i>
+            )}
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
